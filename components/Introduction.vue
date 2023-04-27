19ecf698b5a6e3325e42b5fbebf9ef4182e3a81f
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-04-27 12:37:06 +0200

    setup introduction component structure

---
+++ components/Introduction.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="introduction">
+        <article class="intro-textside">
+
+        </article>
+        <figure class="intro-imageside">
+            <img class="intro-img">
+        </figure>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.introduction {}
+
+.intro-textside {}
+
+.intro-imageside {}
+
+.intro-img {}
+</style>
