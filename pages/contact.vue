aa39372b11d32a4fd9936f22f7e661064458a2f9
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-04-28 12:52:55 +0200

    added heading and intro text to page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,10 @@
 <template>
     <NavBar></NavBar>
-    <h1 class="page-heading"> Contact </h1>
+    <main class="contact-page">
+        <h1 class="page-heading"> Get in touch with me </h1>
+        <p class="page-heading-smalltext">Want to have a talk with me about a new challenge? <br>
+            Feel free to contact me on my email or social media! </p>
+    </main>
 </template>
 
 <script>
@@ -10,4 +14,25 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.contact-page {
+    margin-top: 60px;
+}
+
+.page-heading-smalltext {
+    font-family: var(--normalfont);
+    color: var(--white);
+    font-size: 21px;
+    margin: 0;
+    margin-top: 20px;
+    margin-left: 80px;
+    width: 60%;
+}
+
+/* -------------------------------------------- media queries */
+@media screen and (max-width: 768px) {
+    .contact-page {
+        margin-top: 0;
+    }
+}
+</style>
