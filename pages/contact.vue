bc210c51feaed45be890457beca9f112d80f28ed
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-05-01 15:25:49 +0200

    made contact page responsive

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,9 +2,10 @@
     <NavBar></NavBar>
     <main class="contact-page">
         <h1 class="page-heading"> Get in touch with me </h1>
-        <p class="page-heading-smalltext">Want to have a talk with me about a new challenge? <br>
-            Feel free to contact me on my email or social media! </p>
-
+        <article class="contact-text">
+            <p class="page-heading-smalltext">Want to have a talk with me about a new challenge? <br>
+                Feel free to contact me on my email or social media! </p>
+        </article>
         <section class="contact-info">
             <div class="email-side">
                 <h2 class="contact-info-h2"> Email </h2>
@@ -33,9 +34,18 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .contact-page {
     margin-top: 60px;
+}
+
+.page-heading {
+    font-family: var(--headingfont);
+    color: var(--white);
+    font-size: 50px;
+    font-weight: 500;
+    margin-left: 80px;
+    font-style: italic;
 }
 
 .page-heading-smalltext {
@@ -46,6 +56,7 @@ export default {
     margin-top: 20px;
     margin-left: 80px;
     width: 60%;
+    font-weight: normal;
 }
 
 .contact-info {
@@ -105,6 +116,52 @@ export default {
 @media screen and (max-width: 768px) {
     .contact-page {
         margin-top: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .page-heading {
+        font-size: 50px;
+        font-weight: 500;
+        margin-left: 0;
+        width: 90%;
+        text-align: center;
+
+    }
+
+    .contact-text {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .page-heading-smalltext {
+        font-size: 21px;
+        margin: 0;
+        margin-top: 20px;
+        width: 80%;
+        margin-left: 0;
+    }
+
+    .contact-info {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-bottom: 60px;
+    }
+
+    .email-side {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+
+    .socialmedia-side {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-top: 55px;
     }
 }
 </style>
