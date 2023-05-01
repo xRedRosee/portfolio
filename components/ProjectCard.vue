4a392197db8c52ed154ccca22721496c476b644b
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-05-01 15:33:11 +0200

    setup projectcard component

---
+++ components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <li class="project-card">
+        <img class="project-img">
+        <figcaption class="project-text">
+            <h2 class="project-name"></h2>
+            <a href="" class="see-more"></a>
+        </figcaption>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "ProjectCard"
+}
+</script>
+
+<style scoped>
+.project-card {
+    margin: 0;
+    padding: 0;
+}
+
+.project-img {}
+
+.project-text {}
+
+.project-name {}
+
+.see-more {}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,13 +1,28 @@
 <template>
     <NavBar></NavBar>
     <h1 class="page-heading"> Projects </h1>
+    <section class="projects">
+        <ul class="project-container">
+            <ProjectCard></ProjectCard>
+        </ul>
+    </section>
 </template>
 
 <script>
 import NavBar from '/././components/NavBar.vue';
+import ProjectCard
+    from '~/components/ProjectCard.vue';
 export default {
 
 }
 </script>
 
-<style></style>
+<style>
+.projects {}
+
+.projects-contrainer {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+</style>
