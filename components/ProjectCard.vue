6c051af10c46d3de478425021567d600eea29ed6
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-05-02 14:03:57 +0200

    made modal function

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -3,11 +3,11 @@
         <img class="project-img" :src="Img">
         <figcaption class="project-text">
             <h2 class="project-name"> {{ ProjectName }}</h2>
-            <button class="see-more"> See more &rarr;</button>
+            <button class="see-more" @click="SeeDetail()"> See more &rarr;</button>
         </figcaption>
     </li>
-    <div class="project-detail">
-        <button class="close-btn"> &#10005; </button>
+    <div class="project-detail" v-if="modalAlreadyOpen">
+        <button class="close-btn" @click="CloseModal()"> &#10005; </button>
         <h1 class="detail-name"> {{ ProjectName }}</h1>
         <div class="detail-section">
             <figure class="img-container">
@@ -24,6 +24,11 @@
 <script>
 export default {
     name: "ProjectCard",
+    data() {
+        return {
+            modalAlreadyOpen: false,
+        };
+    },
     props: {
         Img: {
             type: String,
@@ -37,6 +42,16 @@ export default {
         },
         ProjectTools: String,
         ProjectDesc: String,
+    },
+    methods: {
+        SeeDetail() {
+            // open modal
+            this.modalAlreadyOpen = true;
+        },
+        CloseModal() {
+            // close modal
+            this.modalAlreadyOpen = false;
+        }
     }
 }
 </script>
