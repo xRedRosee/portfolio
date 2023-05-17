fbec7fbc17cd7a9577fe7e61e36476506e481ca4
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-05-17 10:10:15 +0200

    updated link

--- pages/project/restyled-stradivarius-homepage.vue
+++ pages/project/restyled-stradivarius-homepage.vue
@@ -1,7 +1,8 @@
 <template>
     <ProjectDetails ProjectName="Restyled Stradivarius homepage" Mockup="/./Projects/stradivariuswebsite-mockup.jpg"
         ProjectTools="#HTML #CSS #JavaScript #JSON"
-        ProjectDesc="Stradivarius is one of my favorite shopping stores. For a challenge of at study I recreated and restyled the homepage of the Stradivarius website. I recreated the navigation function and the slideshow at the top but changed the rest of the homepage. The slideshow of the website is created using Glide.">
+        ProjectDesc="Stradivarius is one of my favorite shopping stores. For a challenge at my study I recreated and restyled the homepage of the Stradivarius website. I recreated the navigation function and the slideshow at the top but changed the rest of the homepage. The slideshow of the website is created using Glide. I used a JSON file and fetch function to display clothes on the webpage. The webpage has different type of slideshows, to make it more interactive for the user."
+        ProjectLink="https://github.com/xRedRosee/stradivarius-recreation">
     </ProjectDetails>
 </template>
 <script>
