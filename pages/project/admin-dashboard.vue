d255aad8da229681f2de713d2597097f8cbda718
LAPTOP-4V8ET9Q2\irisr <irisrn2004@gmail.com>
2023-10-10 13:22:29 +0200

    fixed links

--- pages/project/admin-dashboard.vue
+++ pages/project/admin-dashboard.vue
@@ -2,7 +2,7 @@
     <ProjectDetails ProjectName="Dummy JSON admin dashboard" Mockup="/./Projects/dummyjson-mockup.jpg"
         ProjectTools="#HTML #CSS #JavaScript #JSON"
         ProjectDesc="With the use of the JSON API DummyJSON I created a admin dashboard. The admin can see the users of their website and search for specific user. There's also a product page with all the products on the webshop, and can be filitered by category. This website was created for a school project."
-        ProjectLink="https://github.com/xRedRosee/car-display">
+        ProjectLink="https://github.com/xRedRosee/DummyJSON-Admin-dashboard">
     </ProjectDetails>
 </template>
 <script>
